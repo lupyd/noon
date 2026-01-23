eb4592f40327a4595c9afc546a58a1be65649391
ash-hashtag <hashtag438@gmail.com>
2026-01-23 15:21:18 +0530

    basic api

--- src/main.rs
+++ src/main.rs
@@ -13,66 +13,28 @@ fn main() {
     env_logger::init();
     log::info!("Hello, world!");
 
-    let bits = 2048;
-    let mut rng = rand::rngs::OsRng::default();
-
-    let priv_key = RsaPrivateKey::new(&mut rng, bits).unwrap();
-    let pub_key = priv_key.to_public_key();
-    let n = pub_key.n();
-    let e = pub_key.e();
-    let d = priv_key.d();
-
-    let n = BigUint::from_bytes_le(n.to_bytes_le().as_slice());
-    let e = BigUint::from_bytes_le(e.to_bytes_le().as_slice());
-    let d = BigUint::from_bytes_le(d.to_bytes_le().as_slice());
-
-    log::info!("n: {}", n);
-    log::info!("e: {}", e);
-    log::info!("d: {}", d);
-
-    let m = rng.gen_biguint(1024);
-
-    log::info!("m: {}", m);
-    let r = loop {
-        let r = rng.gen_biguint_below(&n);
-
-        if r != BigUint::zero() && r.gcd(&n) == BigUint::one() {
-            break r;
-        }
-    };
-    log::info!("r: {}", r);
-
-    let r_e = r.modpow(&e, &n);
-    let m_blinded = (&m * &r_e) % &n;
-
-    let s_blinded = m_blinded.modpow(&d, &n);
-
-    let r_inv = r.mod_inverse(&n).unwrap().to_biguint().unwrap();
-
-    let s = (&s_blinded * &r_inv) % &n;
-
-    let m_check = s.modpow(&e, &n);
-
-    assert_eq!(m, m_check);
-
-    log::info!("r_e: {}", r_e);
-    log::info!("m_blinded: {}", m_blinded);
-    log::info!("s_blinded: {}", s_blinded);
-    log::info!("r_inv: {}", r_inv);
-    log::info!("s: {}", s);
-    log::info!("m_check: {}", m_check);
-
     let alice = Client::new("alice");
     let server = Server::new();
 
     let server_public_key = server.public_key();
     let blinded_message =
         alice.create_blinded_message("I vote for freedom".as_bytes(), &server_public_key);
-    let blinded_signature = server.blind_sign(&blinded_message.blinded_message());
+
+    log::info!(
+        "Message: {:X?}, Blinded Message: {:X?}",
+        blinded_message.message(),
+        blinded_message.blinded_message()
+    );
+    let blinded_signature = server
+        .blind_sign(&blinded_message.blinded_message(), &alice.identity)
+        .unwrap();
+    log::info!("Blinded Signature: {:X?}", blinded_signature);
     let signature =
         alice.unblind_signature(&blinded_message, &blinded_signature, &server_public_key);
+    log::info!("Signature: {:X?}", signature);
 
     assert!(server.verify(&blinded_message.message(), &signature));
+    assert!(!server.verify(&blinded_message.message(), &blinded_signature));
 }
 
 struct Server {
@@ -97,14 +59,18 @@ fn verify_identity(identity: &str) -> bool {
 }
 
 impl Server {
-    fn blind_sign(&self, payload: &[u8]) -> Vec<u8> {
+    fn blind_sign(&self, payload: &[u8], identity: &str) -> Result<Vec<u8>, String> {
+        if !verify_identity(identity) {
+            return Err("Invalid identity".to_string());
+        }
+
         let m_blinded = rsa::BigUint::from_bytes_le(payload);
         let d = self.private_key.d();
         let n = self.private_key.n();
 
         let s_blinded = m_blinded.modpow(&d, &n);
 
-        s_blinded.to_bytes_le()
+        Ok(s_blinded.to_bytes_le())
     }
 
     fn verify(&self, payload: &[u8], signature: &[u8]) -> bool {
@@ -122,6 +88,10 @@ struct Client {
 }
 
 impl Client {
+    fn identity(&self) -> &str {
+        &self.identity
+    }
+
     fn new(identity: impl Into<String>) -> Self {
         Self {
             identity: identity.into(),
@@ -163,6 +133,7 @@ impl Client {
     }
 }
 
+#[derive(Debug)]
 struct BlindedMessage {
     m: BigUint,
     r: BigUint,
